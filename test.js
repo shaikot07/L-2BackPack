d7462c8223172fa5e36868148d38091459cc2cea
shaikot07 <saifulislamshaikat007@gmail.com>
2025-05-17 22:00:59 +0600

    updated

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
 const n=10
 console.log(n);
+
+function newFunction (name, age) {
+    const result= name + age
+    return result
+}
+
+console.log(newFunction("shaikot", 25));
+
+const anonummasfunc=()=>{
+    
+}
