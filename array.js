7111cb12025f352b5b999b9a07f926cd143ec890
shaikot07 <saifulislamshaikat007@gmail.com>
2025-05-22 23:31:48 +0600

    play with function

--- array.js
+++ array.js
@@ -31,4 +31,33 @@ function bubbleSort(arr) {
 
 
 let numbers = [64, 34, 25, 12, 22, 11, 90];
-console.log(bubbleSort(numbers)); 
+// console.log(bubbleSort(numbers)); 
+
+const PREPARATION_MINUTES_PER_LAYER = 2;
+const EXPECTED_MINUTES_IN_OVEN = 40;
+
+/**
+ * Determines the number of minutes the lasagna still needs to remain in the oven.
+ *
+
+ */
+export function remainingMinutesInOven(actualMinutesInOven) {
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+}
+
+/**
+ * Given a number of layers, determines the total preparation time.
+
+ */
+export function preparationTimeInMinutes(numberOfLayers) {
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
+}
+
+/**
+ * Calculates the total working time. (prep time + oven time)
+ *
+\
+ */
+export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+}
