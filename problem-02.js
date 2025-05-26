8ccd88a266e0f7b4a2701c7762d6f9fdd5ec4efe
shaikot07 <saifulislamshaikat007@gmail.com>
2025-05-26 22:47:50 +0600

    solve problem 02

---
+++ problem-02.js
@@ -0,0 +1,36 @@
+export function canExecuteFastAttack(knightIsAwake) {
+   return !knightIsAwake;
+}
+/**
+ * A useful spy captures information, which they can't do if everyone's asleep.
+ *
+
+ */
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+    return knightIsAwake || archerIsAwake || prisonerIsAwake;
+}
+/**
+ * You'll get caught by the archer if you signal while they're awake.
+ *
+
+ */
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+return !archerIsAwake && prisonerIsAwake;
+}
+/**
+ * The final stage in the plan: freeing Annalyn's best friend.
+ *
+ 
+ */
+export function canFreePrisoner( 
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent,
+ ) {
+  if (petDogIsPresent) {
+    return !archerIsAwake;
+  } else {
+    return prisonerIsAwake && !knightIsAwake && !archerIsAwake;
+  };
+}
